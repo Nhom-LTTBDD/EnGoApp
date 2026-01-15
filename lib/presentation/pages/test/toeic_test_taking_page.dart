77ef201b5ca992686add9bd594428fcc0c2614f9
Hoang Phuong <155792523+DiazMarco118@users.noreply.github.com>
2026-01-15 16:57:35 +0700

    Fix time của toeic test

--- lib/presentation/pages/test/toeic_test_taking_page.dart
+++ lib/presentation/pages/test/toeic_test_taking_page.dart
@@ -731,9 +731,15 @@ class _ToeicTestTakingPageState extends State<ToeicTestTakingPage> {
 
   String _formatDuration(Duration duration) {
     String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final minutes = twoDigits(duration.inMinutes.remainder(60).toInt());
-    final seconds = twoDigits(duration.inSeconds.remainder(60).toInt());
-    return '$minutes:$seconds';
+    final hours = duration.inHours;
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+
+    if (hours > 0) {
+      return '$hours:$minutes:$seconds';
+    } else {
+      return '$minutes:$seconds';
+    }
   }
 
   void _showExitConfirmation(BuildContext context) {
